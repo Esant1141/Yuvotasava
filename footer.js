e30ab1b5e4175f8d615869167d6f531511ccc82e
Esant1141 <110063894+Esant1141@users.noreply.github.com>
2023-01-29 13:52:22 +0530

    first commit

---
+++ footer.js
@@ -0,0 +1,133 @@
+document.write(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css1/footer.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start text-muted ftr">
+      <!-- Section: Social media -->
+      <br />
+      <hr style="height: 2.5px;color:gray">
+     
+      <!-- Section: Social media -->
+
+      <!-- Section: Links  -->
+      <section class="">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 mx-auto mb-4">
+              <!-- Content -->
+              <p class="text-uppercase mb-4 underline-text a1">Reaches Us</p>
+              <div
+                class="elementor-element elementor-element-71177284 elementor-widget elementor-widget-google_maps"
+                data-id="71177284"
+                data-element_type="widget"
+                data-widget_type="google_maps.default"
+              >
+                <div class="elementor-widget-container">
+                  <style>
+                    /*! elementor - v3.9.2 - 21-12-2022 */
+                    .elementor-widget-google_maps .elementor-widget-container {
+                      overflow: hidden;
+                    }
+                    .elementor-widget-google_maps iframe {
+                      height: 200px;
+                      width: 260px;
+                    }
+                  </style>
+                  <div class="elementor-custom-embed">
+                    <iframe
+                      frameborder="0"
+                      scrolling="no"
+                      marginheight="0"
+                      marginwidth="0"
+                      src="https://maps.google.com/maps?q=Nagarjuna%20College%20Of%20Engineering%20%26%20Technology&#038;t=m&#038;z=9&#038;output=embed&#038;iwloc=near"
+                      title="Nagarjuna College Of Engineering &amp; Technology"
+                      aria-label="Nagarjuna College Of Engineering &amp; Technology"
+                    ></iframe>
+                  </div>
+                  <div class="icons">
+                    <a href="#" class="fa fa-facebook"></a>  
+                    <a href="#" class="fa fa-twitter"></a>  
+                    
+    <a href="#" class="fa fa-instagram"></a>  
+    <a href="#" class="fa fa-youtube"></a>  
+                  </div>
+                </div>
+                
+              </div>
+             
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-5 d-flex">
+              <!-- Links -->
+                <img src="images1/logo3.png" alt="" class="ftr-img1">
+                <img src="images1/logo1.png" alt="" class="ftr-img">
+                <img src="images1/logo2.png" alt="" class="ftr-img">
+        
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 mx-auto mb-md-0 mb-4 text-white ttop">
+              <!-- Links -->
+              <p class="text-uppercase mb-4 underline-text1 a1">College Address</p>
+
+              <p><i class="fa fa-phone" style="font-size:24px"></i> 
+                080-267462700</p>
+              <p>
+                <i class="fa fa-envelope me-3"></i>info@ncetmail.com
+               
+              </p>
+              <p><i class="fa fa-phone" style="font-size:24px"></i> 
+                080-267462700</p>
+              <p>
+              <p><i class="fa fa-map-marker" style="font-size:24px"></i> Mudugurki, Venkatagiri Kote Post, Devanahalli, Bengaluru- 562164.</p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <hr style="height: 2.5px;color:gray">
+      <div
+        class="text-center text-white p-4"
+        
+      >
+      Copyright © 2022 Nagarjuna Group Of Institutions. All Rights Reserved.
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+  </body>
+</html>
+`)
